68e84203a25a07d08dbc45f71b285b04adee11d5
maythukyaw12345 <maythu@maymyanmarlin.com>
2022-04-19 15:20:43 +0630

    divide header footer

---
+++ header.php
@@ -0,0 +1,17 @@
+ <div class="header-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="logo">
+                            <a href="index.html"><img src="images/logo.png" alt=""></a>
+                        </div><!-- end logo -->
+                    </div>
+                </div><!-- end row -->
+            </div><!-- end header-logo -->
+        </div><!-- end header -->
+
+        <header class="header">
+            <div class="container">
+                <?php include_once 'navigation.php' ?>
+            </div><!-- end container -->
+        </header><!-- end header -->
